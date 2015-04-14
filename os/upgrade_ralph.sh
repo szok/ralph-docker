683005b0aa2f8b10fc42247b896be317dafbe925
Piotr Jarolewski <piotr.jarolewski@allegrogroup.com>
2015-04-14 11:10:42 +0200

    Add generated demo data

--- os/upgrade_ralph.sh
+++ os/upgrade_ralph.sh
@@ -9,6 +9,6 @@ echo 'Waiting 10 secs for mysqld to come up'
 sleep 10
 
 /home/ralph/bin/ralph migrate
-DJANGO_SETTINGS_MODULE=ralph.settings /home/ralph/bin/python /home/ralph/fixtures.py
+DJANGO_SETTINGS_MODULE=ralph.settings /home/ralph/bin/ralph make_demo_data -d assets_dc_assets -d relations
 /home/ralph/bin/ralph collectstatic -l 
 mysqladmin shutdown
